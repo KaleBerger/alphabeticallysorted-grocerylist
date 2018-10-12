b5fad08650bc2fa5def274144cd0eb216d91e6a1
KaleB-CS30 <43385287+KaleBerger@users.noreply.github.com>
2018-10-12 13:34:45 -0600

    Update SortingSummative.py

--- SortingSummative.py
+++ SortingSummative.py
@@ -5,7 +5,7 @@
 #Sorting Summative
 #######################################
 
-def bubbleSort(GroceryList):
+def bubbleSort(GroceryList): #funtion to sort the list alphabetically
     for passnum in range(len(GroceryList)-1,0,-1):
         for i in range (passnum):
             if GroceryList[i]>GroceryList[i+1]:
@@ -17,7 +17,7 @@ GroceryList = ["milk"]
 #This will be the dictionary that will use user input to add items to this list
 #will be sorted by food group using quick sort
 
-def addtolist():
+def addtolist():#function to add items to the list
     print("This is your current grocery list:", GroceryList)
     additem = input("Do you want to add an item?: ")
     while additem == ("yes"):
@@ -28,7 +28,7 @@ def addtolist():
         print("Your full list is:", GroceryList)
         
         
-def removefromlist():   
+def removefromlist(): #Function to remove items from the list  
     print("You have went shopping and bought a couple items from your list.")
     keepremoving =input("do you want to remove an item?: ")
     while keepremoving == ("yes"):
@@ -38,7 +38,7 @@ def removefromlist():
     else:
         print ("On your grocery list, remains these items:", GroceryList)
 
-def changeitemonlist():
+def changeitemonlist(): #Function to edit errors in the list
     print ("You realize that you wrote down a wrong item on your list *gasp*!")
     changeitem = input("Do you want to change an item?: ")
     while changeitem == ("yes"):
@@ -53,7 +53,7 @@ def changeitemonlist():
 addtolist()
 removefromlist()
 changeitemonlist()
-bubbleSort(GroceryList)
-print("This is your current alphabetically sorted grocery list:", GroceryList)
+bubbleSort(GroceryList)#sorts the list
+print("This is your current alphabetically sorted grocery list:", GroceryList)#prints the alphabetically sorted final list
     
 
